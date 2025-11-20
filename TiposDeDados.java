01891915838406ebe3dcedee39abcc09e942dd8f
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-20 12:32:23 -0300

    Tipos de Dados Primitivos em Java

---
+++ TiposDeDados.java
@@ -0,0 +1,17 @@
+public class TiposDeDados {
+    public static void main(String[] args) {
+        
+        String texto = "Olá, Mundo!";
+
+        int numeroInteiro = 10;
+
+        float numeroFloat = 10.6f;
+
+        boolean valorBooleano = true;
+
+        System.out.println(texto);
+        System.out.println("Inteiro: " + numeroInteiro);
+        System.out.println("Flutuante: " + numeroFloat);
+        System.out.println("Booleano: " + valorBooleano);
+    }
+}
