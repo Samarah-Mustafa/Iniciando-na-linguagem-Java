382e1ab349392a70ab8caa63943758c93f96d62f
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-20 13:32:43 -0300

    Primeiro Hello World em Java

--- 001/HelloWorld.java
+++ 001/HelloWorld.java
@@ -1,5 +1,5 @@
 //Primeiro Hello World em Java
-public class HelloWorld_001 {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
