d8b596a25a75ebe46cd50a20ca7a87b1ca5b8b59
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-22 17:38:34 -0300

    Estrutura Condicional Simples

--- 004/EstruturaCondicionalSimples.java
+++ 004/EstruturaCondicionalSimples.java
@@ -2,7 +2,7 @@ public class EstruturaCondicionalSimples {
     public static void main(String[] args) {
         int nota = 8;
 
-        if(nota>=7){
+        if (nota >= 7) {
             System.out.println("Aprovado");
         }
     }
