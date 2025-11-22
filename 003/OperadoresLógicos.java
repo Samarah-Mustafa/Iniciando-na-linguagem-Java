ac3451a7ef9f8ccb53819f8d28967e64d4760026
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-22 11:30:46 -0300

    Operadores Lógicos

---
+++ 003/OperadoresLógicos.java
@@ -0,0 +1,13 @@
+public class OperadoresLógicos {
+    public static void main(String[] args) {
+        int a, b;
+        a = 4;
+        b = 8;
+
+        System.out.println(a > b); // 
+        System.out.println(a > b || a < b); 
+        System.out.println(b == 8 && a > b);
+        System.out.println(!(a > b));
+    }
+}
+
