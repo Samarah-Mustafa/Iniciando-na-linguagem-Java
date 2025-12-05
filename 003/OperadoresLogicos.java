ac79044dfb7594d43292d2e54e839703d62affcb
Samarah Mustafá <samarah.dev@gmail.com>
2025-12-05 10:43:12 -0300

    Operadores Lógicos

--- 003/OperadoresLogicos.java
+++ 003/OperadoresLogicos.java
@@ -1,13 +1,12 @@
-public class OperadoresLogicos {
+public class OperadoresLogicos {// Operadores Lógicos: && AND (E), || OR (OU), ! NOT (NÃO)
     public static void main(String[] args) {
         int a, b;
         a = 4;
         b = 8;
 
-        System.out.println(a > b); // 
-        System.out.println(a > b || a < b); 
-        System.out.println(b == 8 && a > b);
-        System.out.println(!(a > b));
+        System.out.println(a > b); // false - não é maior que b.
+        System.out.println(a > b || a < b); // (OR) true - embora a não seja maior que b, a é menor que b.
+        System.out.println(b == 8 && a > b);// (AND)false - b é igual a 8, mas a não é maior que b.
+        System.out.println(!(a > b));// (NOT) true - a expressão (a > b) é false, e o operador ! inverte para true.
     }
 }
-
