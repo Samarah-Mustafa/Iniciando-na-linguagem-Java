71becae67de80816b061308e421afdc1662ce50b
Samarah Mustafá <samarah.dev@gmail.com>
2025-12-05 15:07:08 -0300

    Operadores Aritméticos

---
+++ 003/OperadoresAritmeticos.java
@@ -0,0 +1,15 @@
+public class OperadoresAritmeticos {// Operadores Aritméticos: +, -, *, /, %
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = 20;
+
+        System.out.println("Soma: " + (a + b)); // Saída: Soma: 30
+        System.out.println("Subtração: " + (a - b)); // Saída: Subtração: -10
+        System.out.println("Multiplicação: " + (a * b)); // Saída: Multiplicação: 200
+        System.out.println("Divisão: " + (b / a)); // Saída: Divisão: 2
+        System.out.println("Módulo (Resto da Divisão): " + (b % a)); // Saída: Módulo (Resto da Divisão): 0
+    }
+
+    
+}
