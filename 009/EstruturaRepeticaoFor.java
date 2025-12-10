232c4fca5aa64b3bbf22b023f17f47c906349a8d
Samarah Mustafá <samarah.dev@gmail.com>
2025-12-10 18:40:00 -0300

    Estrutura de Repetição com Laço for

---
+++ 009/EstruturaRepeticaoFor.java
@@ -0,0 +1,9 @@
+public class EstruturaRepeticaoFor {
+    public static void main(String[] args) {
+        for(int i=0; i<10; i++){//Dentro do intervalo de 0 a 9, repita o bloco de código abaixo
+            System.out.println(i + "  Hello, World!");//Imprima o valor de i seguido da mensagem "Hello, World!"
+        }
+    }
+}
+// O loop "for" inicia com i=0 e incrementa i em 1 a cada iteração, até que i seja igual a 10.
+//Esse laço é utilizado quando se tem um intervalo bem definido de dados para percorrer.
