1f759ee63b586f4969d455071ed63f92d5edab37
Samarah Mustafá <samarah.dev@gmail.com>
2026-01-10 12:38:34 -0300

    Vetor

---
+++ 014/Vetor.java
@@ -0,0 +1,14 @@
+public class Vetor { // Como se fosse um prédio com apartamentos
+    public static void main(String[] args) {
+        int valor; // Declarando uma variável simples, como se fosse um apartamento
+        int[] dados = new int[5]; // Vetor, como se fosse um prédio com 5 apartamentos
+
+        dados[2] = 9; // Atribuindo valor ao apartamento 2
+        dados[3] = 7;
+        dados[0] = 6;
+
+        System.out.println("Valor no apartamento 2: " + dados[2]); // Acessando o valor do apartamento 2
+
+
+    }
+}
